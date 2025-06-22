85597be82bba041a767e8dd2c5b21a408cbdfbb8
Mike O'Toole <mike.otoole@gmail.com>
2025-06-21 22:01:49 -0400

    updating install

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -sf "${SCRIPT_DIR}/.gitconfig" "~/.gitconfig"
-ln -sf "${SCRIPT_DIR}/.vimrc" "~/.vimrc"
-ln -sf "${SCRIPT_DIR}/.tmux.conf" "~/.tmux.conf"
+ln -sf "${SCRIPT_DIR}/.gitconfig" "${HOME}/.gitconfig"
+ln -sf "${SCRIPT_DIR}/.vimrc" "${HOME}/.vimrc"
+ln -sf "${SCRIPT_DIR}/.tmux.conf" "${HOME}/.tmux.conf"
